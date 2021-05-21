a0a935d48fdcda7067cc4c61ea6df1d5e21b3e01
PARK HEESEON <ggxz88@naver.com>
2021-05-21 23:42:03 +0900

    이것이 자바다 - 변수와 타입(2)(타입 변환)

---
+++ ThisIsJava/chap02/src/chap02/section03/PromotionExample.java
@@ -0,0 +1,23 @@
+package chap02.section03;
+
+public class PromotionExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		byte byteValue = 10;
+		int intValue = byteValue;		// int <- value
+		System.out.println(intValue);
+		
+		char charValue = '가';
+		intValue = charValue;			// int <- char
+		System.out.println("가의 유니코드: " + intValue);
+		
+		intValue = 500;
+		long longValue = intValue;		// int <- char
+		System.out.println(longValue);
+		
+		intValue = 200;
+		double doubleValue = intValue;	// double <- int
+		System.out.println(doubleValue);
+	}
+}
