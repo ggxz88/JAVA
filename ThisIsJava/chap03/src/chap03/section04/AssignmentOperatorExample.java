463003a31065fe4968e84c0faa03d054345f1680
PARK HEESEON <ggxz88@naver.com>
2021-05-23 00:03:09 +0900

    이것이 자바다 - 연산자(2)(이항 연산자(2), 삼항 연산자)

---
+++ ThisIsJava/chap03/src/chap03/section04/AssignmentOperatorExample.java
@@ -0,0 +1,20 @@
+package chap03.section04;
+
+public class AssignmentOperatorExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int result = 0;
+		result += 10;
+		System.out.println(result);	//10
+		result -= 5;
+		System.out.println(result);	//5
+		result *= 3;
+		System.out.println(result);	//15
+		result /= 5;
+		System.out.println(result);	//3
+		result %= 3;
+		System.out.println(result);	//0
+	}
+
+}
