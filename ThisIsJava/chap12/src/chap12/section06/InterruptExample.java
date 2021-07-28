485a5a4def625838087bfd14aef46e7c4b2a6fe0
PARK HEESEON <ggxz88@naver.com>
2021-07-28 22:50:50 +0900

    이것이 자바다 - 멀티스레드(3)

---
+++ ThisIsJava/chap12/src/chap12/section06/InterruptExample.java
@@ -0,0 +1,19 @@
+package chap12.section06;
+
+public class InterruptExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Thread thread = new PrintThread2();
+		thread.start();
+		
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			
+		}
+		
+		thread.interrupt();
+	}
+	
+}
---
+++ ThisIsJava/chap12/src/chap12/section06/ProducerThread.java
@@ -0,0 +1,17 @@
+package chap12.section06;
+
+public class ProducerThread extends Thread {
+	private DataBox dataBox;
+	
+	public ProducerThread(DataBox dataBox) {
+		this.dataBox = dataBox;
+	}
+	
+	@Override
+	public void run() {
+		for(int i = 1; i <= 3; i++) {
+			String data = "Data-" + i;
+			dataBox.setData(data);
+		}
+	}
+}
